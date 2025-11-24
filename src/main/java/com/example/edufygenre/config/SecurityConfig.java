f6c8250d96d92f04dfbc6bfcd8187bac275b2faf
Alex <alexander.westerberg.svedberg@gmail.com>
2025-11-24 20:17:05 +0100

    added JwtAuthConverter removed /api/v1 from controller. updated keycloak address in app.prop

--- src/main/java/com/example/edufygenre/config/SecurityConfig.java
+++ src/main/java/com/example/edufygenre/config/SecurityConfig.java
@@ -1,18 +1,26 @@
 package com.example.edufygenre.config;
 
+import com.example.edufygenre.converters.JwtAuthConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 // ED-42-AWS
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)    //ED-349-AWS
 public class SecurityConfig {
 
+//ED-349-AWS
+    private final JwtAuthConverter jwtAuthConverter;
 
-        //TODO Inte klar
+    @Autowired
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -21,7 +29,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/h2-console/**").permitAll()
-                                .anyRequest().permitAll()
+                                .anyRequest().authenticated()
+                )       //ED-349-AWS
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
                 );
 
         return http.build();
